52b2746ef5fb578afbcf4fd780005d255a603bdc
tercsam <mascret.clement@gmail.com>
2024-01-08 22:29:40 +0100

    update file 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -13,4 +13,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
         return (0);
     
     return (node -> left == NULL && node -> right == NULL);
+             return (1);
+    return (0);
 }
