e4aca5d8eaad5dc36500dd09f49228e2f002a44b
tercsam <mascret.clement@gmail.com>
2024-01-08 14:49:33 +0100

    update file 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,29 +5,28 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
  binary_tree_t *new_node;
 
  if (parent == NULL)
-    return(NULL);
+    return (NULL);
 
 new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
-    return(NULL);
+    return (NULL);
 
-new_node-> n = value;
-new_node-> parent = parent;
-new_node-> left = NULL;
+new_node -> n = value;
+new_node -> parent = parent;
+new_node -> left = NULL;
 
-if(parent->right != NULL)
+if (parent->right != NULL)
 {
-    new_node->right = parent->right;
-    parent->right->parent = new_node;
+    new_node -> right = parent->right;
+    parent- > right->parent = new_node;
 }
 
 else
 {
-    new_node->right = NULL;
+    new_node -> right = NULL;
 }
 
-parent->right = new_node;
+parent -> right = new_node;
 
 return (new_node);
 }
-
