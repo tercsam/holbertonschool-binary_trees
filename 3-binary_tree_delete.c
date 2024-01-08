a5c20e34736d0a68bc0836fde9b8c20d3c0a0741
mathpvx <mathilde.pavaux@gmail.com>
2024-01-08 07:12:34 -0800

    fix error

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,11 +6,12 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->left != NULL)
-		binary_tree_delete(tree->left);
+	if (tree == NULL)
+		return;
 
-	if (tree->right != NULL)
-		binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+
+	binary_tree_delete(tree->right);
 
 	free(tree);
 }
