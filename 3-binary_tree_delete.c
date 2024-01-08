9c86e48b33de6dfdf53d987b39d56fbefc1caae2
mathpvx <mathilde.pavaux@gmail.com>
2024-01-08 07:06:18 -0800

    betty check

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_delete - deletes the tree
+ * @tree: pointer to the root of the binary tree
  */
 
 void binary_tree_delete(binary_tree_t *tree)
@@ -11,5 +12,5 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree->right != NULL)
 		binary_tree_delete(tree->right);
 
-	free (tree);
+	free(tree);
 }
