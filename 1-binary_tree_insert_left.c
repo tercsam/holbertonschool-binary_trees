dfc6a421faf3ac0cc369a338e38e01acf7a5e5de
mathpvx <mathilde.pavaux@gmail.com>
2024-01-08 06:29:27 -0800

    fix the code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,6 +3,7 @@
  * binary_tree_insert_left - inserts a node as the left child of another node
  * @parent: pointer to the node to insert left child in
  * @value: data to store in the node
+ * Return: the new node if success, NULL if fails
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -14,15 +15,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
-			return (NULL);
-	
-	if (newNode->left != NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
 	{
 		parent->left->parent = newNode;
 		newNode->left = parent->left;
+		parent->left = newNode;
+		newNode->parent = parent;
 	}
 
 	parent->left = newNode;
+	newNode->parent = parent;
 
 	return (newNode);
 }
