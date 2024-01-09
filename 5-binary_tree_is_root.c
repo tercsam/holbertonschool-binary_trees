b9deab856983dac3091c755ad550977e3e714b84
mathpvx <mathilde.pavaux@gmail.com>
2024-01-09 01:16:44 -0800

    task 4

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: the node to check
+ * Return: 1 if root, 0 if NULL or not the root
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	
+	return (0);
+}
