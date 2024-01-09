d29c3fa579856eda364105797f140d4a203e5a4a
mathpvx <mathilde.pavaux@gmail.com>
2024-01-09 01:06:51 -0800

    betty check task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,19 +3,19 @@
 /**
  * binary_tree_preorder - Goes through a binary tree using pre-order traversal
  * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node. The value in the node must be passed as a parameter to this function.
+ * @func: Pointer to a function to call for each node.
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    func(tree -> n);
+	func(tree->n);
 
-    binary_tree_preorder(tree -> left, func);
+	binary_tree_preorder(tree->left, func);
 
-    binary_tree_preorder(tree -> right, func);
+	binary_tree_preorder(tree->right, func);
 
 }
 
