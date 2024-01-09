cfe8785f3b2f869f1ec02dac1875993929df2741
mathpvx <mathilde.pavaux@gmail.com>
2024-01-09 05:38:56 -0800

    betty check

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,11 +9,11 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree -> left == NULL && tree -> right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    return binary_tree_leaves(tree -> left) + binary_tree_leaves (tree -> right);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
