06630e04fe8b2f09e77b9125ded47f7ef67ff247
mathpvx <mathilde.pavaux@gmail.com>
2024-01-09 01:10:32 -0800

    betty check task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    while (tree -> parent != NULL)
-    {
-        tree = tree -> parent;
-        depth++;
-    }
-    
-    return depth;
+	while (tree->parent != NULL)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+
+	return (depth);
 }
