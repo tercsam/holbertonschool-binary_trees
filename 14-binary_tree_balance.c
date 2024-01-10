a89631792c6676d63a84a79cea562790e56074ef
mathpvx <mathilde.pavaux@gmail.com>
2024-01-10 01:57:34 -0800

    betty check

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
+ * binary_tree_height - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree
  *
  * Return: Balance factor of the binary tree
@@ -9,20 +9,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t leftHeight = 0;
-    size_t rightHeight = 0;
+	size_t leftHeight = 0;
+	size_t rightHeight = 0;
 
-    if(tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if(tree -> left != NULL)
-        leftHeight = binary_tree_height(tree -> left);
+	if (tree->left != NULL)
+		leftHeight = binary_tree_height(tree->left);
 
-    if(tree -> right != NULL)
-        rightHeight = binary_tree_height(tree -> right);
+	if (tree->right != NULL)
+		rightHeight = binary_tree_height(tree->right);
 
-    
-    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;     
+
+	return ((leftHeight > rightHeight ? leftHeight : rightHeight) + 1);
 }
 
 /**
@@ -36,9 +36,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_balance(const binary_tree_t *tree)
 {
 
-    if(tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return binary_tree_height(tree -> left) - binary_tree_height(tree->right);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 }
