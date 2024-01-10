dca5973934c583c80f6307c2c1c5610551f36ae7
mathpvx <mathilde.pavaux@gmail.com>
2024-01-10 03:18:29 -0800

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,7 +16,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->left != 0 || tree->right != 0)
 	{
-		left = binary_tree_nodes(tree->left);	
+		left = binary_tree_nodes(tree->left);
 		right = binary_tree_nodes(tree->right);
 	}
 
