f4f0baaeb25b911d835f7c38c91ffe9d076e2107
tercsam <mascret.clement@gmail.com>
2024-01-10 16:43:39 +0100

    add file 18 but need betty test

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node in a binary tree
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the uncle node, or NULL if node is NULL or has no uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if(node == NULL || node -> parent == NULL || node -> parent -> parent == NULL)
+        return (NULL);
+
+    if(node -> parent -> parent -> left == node -> parent)
+        return (node -> parent -> parent -> right);
+
+    return (node -> parent -> parent -> left);
+}
