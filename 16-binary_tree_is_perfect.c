db891a51f930ba153d396140260a22fee7273913
mathpvx <mathilde.pavaux@gmail.com>
2024-01-10 06:21:22 -0800

    16 betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,10 +11,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	if (tree->left && tree->right == 0)
 		return (1);
-	
+
 	left = binary_tree_height(tree->left) + 1;
 	right = binary_tree_height(tree->right) + 1;
 
@@ -42,7 +42,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left = 0, right = 0;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
